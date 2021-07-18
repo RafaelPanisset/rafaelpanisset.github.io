e6a7e5634bfd06bfb7fa5399822e84ad78a4b0f7
RafaelPanisset <51482528+RafaelPanisset@users.noreply.github.com>
2021-07-18 14:45:50 -0300

    Update palindrome.js

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,13 +1,18 @@
-var palavra = prompt("Digite a palavra: ");
-    
+
+function palindrome(palavra) {
     var N = palavra.length;
     var ok = true;
+    palavra = palavra.toLowerCase();
     for (var i = 0; i < N / 2; i++) {
         if (palavra[i] != palavra[N - i - 1]) {
             ok = false;
         }
     }
-    if (ok)
+    return ok;
+}
+
+var palavra = prompt("Digite a palavra: "); 
+    if (palindrome(palavra))
         alert("É uma palindrome\n");
     else    
         alert("Não é uma palindrome\n");
