5c2ace5472cf002013ac6f5a091e38647afb99e8
RafaelPanisset <51482528+RafaelPanisset@users.noreply.github.com>
2021-07-18 14:42:35 -0300

    Update imparPAr.js

--- js/imparPAr.js
+++ js/imparPAr.js
@@ -1,5 +1,14 @@
- var numero = prompt("Digite o valor: ");
-    if (numero % 2 == 0)
+ 
+ 
+function calcular(resposta) {
+  if (numero % 2 == 0)
+     return true;
+  else
+     return false;
+}
+
+var numero = prompt("Digite o valor: ");
+    if (calcular(2))
         alert("O número é par\n");
     else    
         alert("O número é impar\n");
