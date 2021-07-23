58ee53dc8d84896b2c5d7223e03c06b68b64e3c0
RafaelPanisset <51482528+RafaelPanisset@users.noreply.github.com>
2021-07-23 20:20:28 -0300

    Update emailValidar.js

--- emailValidar.js
+++ emailValidar.js
@@ -1,1 +1,23 @@
-fdfd
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+function emailValidar(email) { 
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+var palavra = prompt("Digite o email: "); 
+if (emailValidar(palavra)) {
+    alert("É email");
+}
+else {
+    alert("Não é email");
+}
+
+
+</script>
+
+</body>
+</html>
