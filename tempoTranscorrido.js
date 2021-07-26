cc0047ba7d8c2e66590d30cdccbd92a94ce0ab18
RafaelPanisset <51482528+RafaelPanisset@users.noreply.github.com>
2021-07-25 21:19:10 -0300

    Update tempoTranscorrido.js

--- tempoTranscorrido.js
+++ tempoTranscorrido.js
@@ -2,13 +2,14 @@ var dataDigitada = prompt("Digite a data: ");
 var dias = dataDigitada.split("/")[0];
 var meses = dataDigitada.split("/")[1];
 var anos = dataDigitada.split("/")[2];
-var formatoAmericano = new Date(anos, meses, dias);
-hoje = new Date();
+var formatoAmericano = new Date(anos, meses - 1, dias);
+var hoje = new Date();
 diferenca = Math.floor(hoje.getTime() - formatoAmericano.getTime());
 var dia  = 1000 * 60 * 60 * 24;
 
-dias = Math.floor(diferenca / dia);
-meses = Math.floor(dias / 31);
-anos = Math.floor(meses/ 12);
-
-alert("Foram: " + anos + " anos " + meses + " e " + dias);
+var diasResposta = Math.floor(diferenca / dia);
+var mesesResposta = Math.floor(diasResposta / 31);
+var anosResposta = Math.floor(mesesResposta/ 12);
+ 
+//alert("Foram: " + anosResposta + " anos " + mesesResposta + " meses  e " + diasResposta);
+alert(diferenca);
